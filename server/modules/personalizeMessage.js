9fd801eae493953c180781395bac847600fc47fd
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-04-24 23:25:44 -0500

    Created helper function 'determineTimeOfDay', which evaluates the time of day for a given date and returns the proper greeting string to reflect the time of day

--- server/modules/personalizeMessage.js
+++ server/modules/personalizeMessage.js
@@ -4,12 +4,39 @@ function personalizeMessage(selectedMessage, guestInformation) {
 
     let personalizedMessage = selectedMessage;
 
-    personalizedMessage = personalizedMessage.replaceAll('#greeting', 'Good morning');
+    let greeting = determineTimeOfDay(guestInformation.startTimestamp);
+
+    personalizedMessage = personalizedMessage.replaceAll('#greeting', greeting);
     personalizedMessage = personalizedMessage.replaceAll('#firstName', guestInformation.firstName);
     personalizedMessage = personalizedMessage.replaceAll('#companyName', guestInformation.companyName);
     personalizedMessage = personalizedMessage.replaceAll('#roomNumber', guestInformation.roomNumber);
 
     return personalizedMessage;
 }
+
+
+// 'determineTimeOfDay' converts epoch to human-readable date,
+// then creates string depending on when the time of day is
+function determineTimeOfDay(number) {
+    let timeOfDay = new Date(number * 1000);
+
+    timeOfDay = timeOfDay.toLocaleString();
+
+    // console.log('timeOfDay', timeOfDay);
+
+    let greeting = '';
+
+    if (timeOfDay.includes('AM')) {
+        greeting = 'Good morning'
+
+    } else if (timeOfDay.includes('PM')) {
+        greeting = 'Good evening'
+    } else {
+        console.log('something is wrong');
+    }
+
+    return greeting;
+}
+
 
 module.exports = personalizeMessage;
