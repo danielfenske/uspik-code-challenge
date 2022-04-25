898d1fae64fcf5ed48dccd5531e6255a5c0dbc2b
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-04-24 21:47:43 -0500

    Removed all unnecessary files; renamed components within server files; added pool.js file to connect with DB; ran server and tested connection

---
+++ server/modules/pool.js
@@ -0,0 +1,25 @@
+const pg = require('pg');
+const Pool = pg.Pool;
+
+const config = {
+    database: 'uspik_interview',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+};
+
+const pool = new Pool(config);
+
+pool.on("connect", () => {
+    console.log('connected to postgreSQL');
+    
+})
+
+pool.on('error', (error) => {
+    console.log('error connecting to postgreSQL', error);
+    
+});
+
+module.exports = pool;
+
---
+++ server/routes/guest.router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const guestRouter = express.Router();
+
+// DB CONNECTION
+const pool = require('../modules/pool');
+
+// GET
+
+
+// POST
+
+
+// PUT
+
+
+// DELETE
+
+module.exports = guestRouter;
